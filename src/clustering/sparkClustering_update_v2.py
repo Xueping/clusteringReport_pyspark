7915b6704a5d8814eb231f63c1c01bc5117902bd
Xueping <pengxueping@gmail.com>
2017-02-10 00:06:32 +1100

    update config file

--- src/clustering/sparkClustering_update_v2.py
+++ src/clustering/sparkClustering_update_v2.py
@@ -128,7 +128,7 @@ if __name__ == "__main__":
             originalFeature.append(cf)
     
     
-    sc = SparkContext("local[10]", "clusteringReport")   
+    sc = SparkContext("clusteringReport")   
     sqlContext = SQLContext(sc)
     
     rawData = sc.textFile(dataFile)
